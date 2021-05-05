5bc75aadbf39ec00d7a23f84bb5fa8d239bb7748
Beck Busch <beckbusch5@gmail.com>
2021-05-05 23:03:46 +1200

    Added proper code stuff like a constants dictionary and client secret reading.

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import discord
 import asyncio
-# Create the client object
+
+# Initialisation and constant definition
 client = discord.Client()
+secret = open('client_secret.txt', 'r').read()
+settings = {"general_channel_id": 762152576032047156,
+            "bot_channel_id": 762224452440031284
+            }
 
 # Startup notification
 @client.event
 async def on_ready():
     print('Logged in as\n' + client.user.name + '\n' + str(client.user.id) + '\n------')
     await client.change_presence(status=discord.Status.online, activity=discord.Game("boof"))
-    await client.get_channel(762152576032047156).send("Krug Bot online!\n*boof boof boof*")
+    await client.get_channel(settings["bot_channel_id"]).send("Krug Bot online!\n*boof boof boof*")
 
 # Reacting to message being sent anywhere in the server
 @client.event
@@ -16,8 +21,8 @@ async def on_message(message):
     code = message.content[-6:]
     line = message.content[:-6]
     if line == "Join my coop game on Bloons TD 6! https://join.btd6.com/Coop/":
-        await client.get_channel(762152576032047156).send(code)
         await discord.Message.delete(message)
+        await client.get_channel(message.channel.id).send(code)
 
 
-client.run()
+client.run(secret)
